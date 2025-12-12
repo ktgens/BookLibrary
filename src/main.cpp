5cae4dca443f33afe3d0511ecc107cb75e850540
ktgens <uluu33697@gmail.com>
2025-12-12 17:22:29 +0300

    Added main menu display.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,35 @@
+#include <iostream>
+#include "Library.h"
+
+void showMenu()
+{
+    std::cout << "=== LIBRARY MANAGEMENT ===" << std::endl;
+    std::cout << "1. View all books\n";
+    std::cout << "2. View all users\n";
+    std::cout << "3. Add new book\n";
+    std::cout << "4. Register user\n";
+    std::cout << "5. Borrow book\n";
+    std::cout << "6. Return book\n";
+    std::cout << "7. Search book by ISBN\n";
+    std::cout << "8. View user profile\n";
+    std::cout << "9. Save data\n";
+    std::cout << "10. Exit\n";
+    std::cout << "Choice: ";
+}
+
+void clearIn() 
+{
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
 int main()
 {
+    Library lib("data/library_data.txt");
+    while(true)
+    {
+        showMenu();
+    }
+
     return 0;
 }
